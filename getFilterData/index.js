bbe8890cef3d2d0fd011a4c79707118ac67868ce
dkonwarprasai <d.konwar.prasai@avanade.com>
2023-05-05 09:09:07 -0400

    update prompt

--- getFilterData/index.js
+++ getFilterData/index.js
@@ -28,12 +28,35 @@ module.exports = async function (context, req) {
     for (let i = 0; i < blobContent.length; i++) {
         const response = await openAI.createCompletion({
             model: 'text-davinci-003',
-            prompt: ``,
-            stop: [],
+            prompt: `The following AI tool helps the IT support team classify an email into labels that are: urgent, non urgent, spam. Questions and concerns are good examples of urgent emails.\n\n` + 
+            //Context Example
+            `User: John Smith\n` +
+            `Subject: Require immediate assistance\n` + 
+            `Body: Need help. Contact asap\n` +
+            `Label: Urgent\n` + 
+            
+            //Context Example
+            `User: Jane Doe\n` + 
+            `Subject: Increase your Website Traffic by 100%\n` + 
+            `Body: Dear Sir/Madam, we are a fast traffic company that wants to provide you this excellent service. Buy it now.\n` + 
+            `Label: Non Urgent\n` + 
+            
+            //Context Example
+            `User: Jeff Bezos\n` + 
+            `Subject: You've won\n` + 
+            `Body: Amazon is sending you a refunding of $88.91. Please click this link immediately. Offer only valid for next 20 minutes.\n` + 
+            `Label: Spam\n` + 
+            
+            //Actual use case
+            `User: ${blobContent[i].email}\n` + 
+            `Subject: ${blobContent[i].subject}\n` + 
+            `Body: ${blobContent[i].body}\n`
+            `Label:`,
+            stop: ["\n", "User:", "Subject:", "Body:", "Label:"],
             max_tokens: 7,
             temperature: 0
         });
-        console.log(`processing ${i} : `);
+        console.log(`processing ${i} : ${blobContent[i].email} | response: ${JSON.stringify(response)}`);
         // TODO: Update the blob file with an additional classification column
         // TODO: Create Cosmos DB records for every iteration if it does not exist
         // TODO: Update the DB records according to the responses
