1d31d4a7d5193269d381a5b52809e5ba491f1295
dkonwarprasai <d.konwar.prasai@avanade.com>
2023-05-04 21:34:28 -0400

    added packages; add blob retrival and openai request

--- getFilterData/index.js
+++ getFilterData/index.js
@@ -1,13 +1,59 @@
+const azureStorage = require('azure-storage');
+const openai = require('openai');
+
+const blobService = azureStorage.createBlobService();
+
+const CONTAINER_NAME = 'aihackathoncontainer';
+
+const { Configuration, OpenAIApi } = openai;
+const configuration = new Configuration({
+    organization: "org-i16GdI3biVetsydNnhzCYRLs",
+    apiKey: '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',
+});
+const openAI = new OpenAIApi(configuration);
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const jsonFileName = req.body.jsonFileName;
+    if (!jsonFileName || jsonFileName === '') {
+        context.log('Error: Filename empty or undefined');
+        return;
+    }
+
+    // Retrieve the JSON file from a storage blob
+    const blobContent = await getBlobContent(CONTAINER_NAME, jsonFileName);
+
+    // Fire off the request to OpenAI, iterate through the json objects
+    for (let i = 0; i < blobContent.length; i++) {
+        const response = await openAI.createCompletion({
+            model: 'text-davinci-003',
+            prompt: ``,
+            stop: [],
+            max_tokens: 7,
+            temperature: 0
+        });
+        console.log(`processing ${i} : `);
+        // TODO: Update the blob file with an additional classification column
+        // TODO: Create Cosmos DB records for every iteration if it does not exist
+        // TODO: Update the DB records according to the responses
+    }
+
+    const responseMessage = "Function executed successfully.";
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
+}
+
+async function getBlobContent(containerName, blobName) {
+    return new Promise((resolve, reject) => {
+      blobService.getBlobToText(containerName, blobName, function(err, blobContent) {
+          if (err) {
+              reject(err);
+          } else {
+              resolve(blobContent);
+          }
+      });
+    });
 }
