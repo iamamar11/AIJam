d0eac9dddf41eb3ffad7ac4df4107ef6ece4282a
iamamar11 <iamamarjit95@gmial.com>
2023-05-05 12:27:13 -0400

    updating the storage package and reading blob code

--- getFilterData/index.js
+++ getFilterData/index.js
@@ -1,78 +1,77 @@
 const { BlobServiceClient } = require("@azure/storage-blob");
 const openai = require('openai');
-require('dotenv').config();
+const dotenv = require('dotenv')
 
-const STORAGE_NAME = 'aihackathon2023';
-const CONTAINER_NAME = 'aihackathoncontainer';
-
-const { Configuration, OpenAIApi } = openai;
-const configuration = new Configuration({
-    organization: process.env.OPEN_AI_ORGANIZATION,
-    apiKey: process.env.OPEN_AI_API_KEY,
-});
-const openAI = new OpenAIApi(configuration);
+dotenv.config()
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const jsonFileName = req.body.jsonFileName;
-    if (!jsonFileName || jsonFileName === '') {
-        context.log('Error: Filename empty or undefined');
+    if (!req.body) {
+        context.res = {
+            status: 404,
+            body: 'Missing jsonFileName.'
+        };
         return;
     }
-    const blobServiceClient = new BlobServiceClient(STORAGE_NAME);
-    const containerClient = blobServiceClient.getContainerClient(CONTAINER_NAME);
-    // Retrieve the JSON file from a storage blob
+
     let blobContent = null;
     try{ 
-        blobContent = await getBlobContent(containerClient, jsonFileName);
+        const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.CONTAINER_CONNECTION_STRING);
+        const containerClient = blobServiceClient.getContainerClient(process.env.CONTAINER_NAME);
+        const blobClient = containerClient.getBlockBlobClient(req.body.jsonFileName);
+        const downloadResponse = await blobClient.download();
+        await streamToString(downloadResponse.readableStreamBody);
     } catch(e) {
-        context.log(`Error: ${e}`);
+        context.res = {
+            status: 404,
+            body: 'Error while reading file'
+        };
         return;
     }
 
     // Fire off the request to OpenAI, iterate through the json objects
-    let newJSONData = [];
-    for (let i = 0; i < blobContent.length; i++) {
-        const response = await openAI.createCompletion({
-            model: 'text-davinci-003',
-            prompt: `The following AI tool helps the IT support team classify an email into labels that are: urgent, non urgent, spam. Questions and concerns are good examples of urgent emails.\n\n` + 
-            //Context Example
-            `User: John Smith\n` +
-            `Subject: Require immediate assistance\n` + 
-            `Body: Need help. Contact asap\n` +
-            `Label: Urgent\n` + 
+   // let newJSONData = [];
+    // for (let i = 0; i < blobContent.length; i++) {
+    //     const response = await openAI.createCompletion({
+    //         model: 'text-davinci-003',
+    //         prompt: `The following AI tool helps the IT support team classify an email into labels that are: urgent, non urgent, spam. Questions and concerns are good examples of urgent emails.\n\n` + 
+    //         //Context Example
+    //         `User: John Smith\n` +
+    //         `Subject: Require immediate assistance\n` + 
+    //         `Body: Need help. Contact asap\n` +
+    //         `Label: Urgent\n` + 
             
-            //Context Example
-            `User: Jane Doe\n` + 
-            `Subject: Increase your Website Traffic by 100%\n` + 
-            `Body: Dear Sir/Madam, we are a fast traffic company that wants to provide you this excellent service. Buy it now.\n` + 
-            `Label: Non Urgent\n` + 
+    //         //Context Example
+    //         `User: Jane Doe\n` + 
+    //         `Subject: Increase your Website Traffic by 100%\n` + 
+    //         `Body: Dear Sir/Madam, we are a fast traffic company that wants to provide you this excellent service. Buy it now.\n` + 
+    //         `Label: Non Urgent\n` + 
             
-            //Context Example
-            `User: Jeff Bezos\n` + 
-            `Subject: You've won\n` + 
-            `Body: Amazon is sending you a refunding of $88.91. Please click this link immediately. Offer only valid for next 20 minutes.\n` + 
-            `Label: Spam\n` + 
+    //         //Context Example
+    //         `User: Jeff Bezos\n` + 
+    //         `Subject: You've won\n` + 
+    //         `Body: Amazon is sending you a refunding of $88.91. Please click this link immediately. Offer only valid for next 20 minutes.\n` + 
+    //         `Label: Spam\n` + 
             
-            //Actual use case
-            `User: ${blobContent[i].email}\n` + 
-            `Subject: ${blobContent[i].subject}\n` + 
-            `Body: ${blobContent[i].body}\n`
-            `Label:`,
-            stop: ["\n", "User:", "Subject:", "Body:", "Label:"],
-            max_tokens: 7,
-            temperature: 0
-        });
-        console.log(`processing ${i} : ${blobContent[i].email} | response: ${JSON.stringify(response)}`);
-        // TODO: Update the blob file with an additional classification column
-        newJSONData.push({
-            User: blobContent[i].email,
-            Subject: blobContent[i].subject,
-            Body: blobContent[i].body,
-            Label: ''
-        });
-    }
+    //         //Actual use case
+    //         `User: ${blobContent[i].email}\n` + 
+    //         `Subject: ${blobContent[i].subject}\n` + 
+    //         `Body: ${blobContent[i].body}\n`
+    //         `Label:`,
+    //         stop: ["\n", "User:", "Subject:", "Body:", "Label:"],
+    //         max_tokens: 7,
+    //         temperature: 0
+    //     });
+    //     console.log(`processing ${i} : ${blobContent[i].email} | response: ${JSON.stringify(response)}`);
+    //     // TODO: Update the blob file with an additional classification column
+    //     newJSONData.push({
+    //         User: blobContent[i].email,
+    //         Subject: blobContent[i].subject,
+    //         Body: blobContent[i].body,
+    //         Label: ''
+    //     });
+    // }
     
     // TODO: Create Cosmos DB records for every iteration if it does not exist
     // TODO: Update the DB records according to the responses
@@ -84,25 +83,16 @@ module.exports = async function (context, req) {
     };
 }
 
-async function getBlobContent(containerClient, blobName) {
-    const blobClient = await containerClient.getBlobClient(blobName);
-
-    const downloadResponse = await blobClient.download();
-
-    const downloaded = await streamToBuffer(downloadResponse.readableStreamBody);
-    console.log('Downloaded blob content:', downloaded.toString());
-    return downloaded.toString();
-}
-
-async function streamToBuffer(readableStream) {
+// Helper function to convert a ReadableStream to a string
+async function streamToString(readableStream) {
     return new Promise((resolve, reject) => {
-        const chunks = [];
-        readableStream.on('data', (data) => {
-            chunks.push(data instanceof Buffer ? data : Buffer.from(data));
-        });
-        readableStream.on('end', () => {
-            resolve(Buffer.concat(chunks));
-        });
-        readableStream.on('error', reject);
+      const chunks = [];
+      readableStream.on("data", (data) => {
+        chunks.push(data.toString());
+      });
+      readableStream.on("end", () => {
+        resolve(chunks.join(""));
+      });
+      readableStream.on("error", reject);
     });
-}
+  }
